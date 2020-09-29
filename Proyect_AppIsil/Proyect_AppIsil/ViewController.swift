1fdfdd27e172e61538341d402264fb110b12b06a
SueOcho <64862065+SueOcho@users.noreply.github.com>
2020-09-29 14:27:13 -0700

    Inicio
    Mucho lag

---
+++ Proyect_AppIsil/Proyect_AppIsil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyect_AppIsil
+//
+//  Created by user179030 on 9/29/20.
+//  Copyright © 2020 user179030. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
